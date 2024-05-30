6731f9059f924413d1dd3562cd3812b556565e18
darshanraok <64535064+darshanraok@users.noreply.github.com>
2024-05-30 17:30:25 +0530

    Create AddTwoInteger.py

---
+++ AddTwoInteger/AddTwoInteger.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def sum(self, num1, num2):
+        """
+        :type num1: int
+        :type num2: int
+        :rtype: int
+        """
+        return num1 + num2
+
+# Example usage:
+solution = Solution()
+print(solution.sum(12, 5))    # Output: 17
+print(solution.sum(-10, 4))   # Output: -6
